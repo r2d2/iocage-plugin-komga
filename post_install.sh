a53cebcfe09dd9c27b355261054a4c977404ec79
r2d2 <nclsmarc@gmail.com>
2023-03-04 16:25:45 +0100

    Correcting init script rights, and correcting the release of komga

--- post_install.sh
+++ post_install.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
-KOMGA_RELEASE="0.162.0"
 cp /usr/local/etc/komga-plugin.conf.sample /usr/local/etc/komga-plugin.conf
 . /usr/local/etc/komga-plugin.conf
 : "${KOMGA_APP_DIR:=/usr/local/komga}"
 
 mkdir -p $KOMGA_APP_DIR
 cd $KOMBA_APP_DIR
-fetch https://github.com/gotson/komga/releases/download/v$KONGA_RELEASE/komga-$KONGA_RELEASE.jar
-ln -sf komga-0.162.0.jar "komga.jar"
+fetch https://github.com/gotson/komga/releases/download/v0.162.0/komga-0.162.0.jar
+ln -sf komga-0.162.0.jar komga.jar
 
 # Start service
 sysrc -f /etc/rc.conf komga_enable="YES"
@@ -17,5 +16,5 @@ service komga start
 {
     echo "✅ komga installation is complete!"
     echo "App dir: $KOMGA_APP_DIR"
-    echo "Komga version: $KOMGA_RELEASE"
+    echo "Komga version: 0.162.0"
 } > /root/PLUGIN_INFO
